505eb455faacf2a19778fa641ae9469885ce4f0b
Mohammed Yasser <mohdyasser100@gmail.com>
2024-12-13 16:42:53 +0200

    add: node.h file

--- Parser.h
+++ Parser.h
@@ -5,26 +5,10 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include "Node.h"
 
 using namespace std;
 
-class Node
-{
-public:
-    string value;
-    string type;
-    Node* sibling;
-    Node* leftChild;
-    Node* rightChild;
-    Node(string value)
-    {
-        this->value = value;
-        this->type = "";
-        this->sibling = NULL;
-        this->leftChild = NULL;
-        this->rightChild = NULL;
-    }
-};
 
 class Parser
 {
